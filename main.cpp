48047d18a32d87b45aa15b7401009f1a3174d44c
Virus2466 <Pateldev2311@protonmail.com>
2024-09-04 18:20:31 +0530

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main()
+{
+    int numberOfStudents = 0;
+
+    std::cout << "How many students in our clas ? : ";
+    std::cin >> numberOfStudents;
+
+    // new keyword is for allocating resources at runtime 
+    int* studentids = new int[numberOfStudents];
+
+
+    for (int i = 0; i < numberOfStudents; i++) {
+        studentids[i] = i;
+    }
+
+    // prevents memory leaks.
+    delete[] studentids;
+
+    return 0;
+
+}
